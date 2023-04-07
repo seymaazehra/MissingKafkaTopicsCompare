f1f033d1cc51561250492b99cfd955a2432ec380
seymaazehra <75252489+seymaazehra@users.noreply.github.com>
2023-04-07 15:57:12 +0300

    Add files via upload

---
+++ CompareMissingTopic.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#read -p "Enter Kafka IP: "  kafkaIP
+#read -p "Enter kafka-console-consumer.sh path: "  kafka_topics_path
+#read -p "Enter kafka_text_file path: "  TextFile
+
+kafkaIP=""
+kafka_topics_path=./kafka-topics.sh
+missing_count=0
+
+
+for topic in $($kafka_topics_path --bootstrap-server $kafkaIP --list)
+do
+      let counter=counter+1
+      
+      if ! grep $topic ./missing_list.txt
+	 then
+      echo "$counter. Topic: '$topic' is missing"
+	  let missing_count=missing_count+1
+	  else
+	  echo "$counter. Topic: '$topic' is exists"
+	 fi
+
+done 
+echo "Total $missing_count topic missing"	  
+  
+     
+ 
+
